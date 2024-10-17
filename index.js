72d7f5c921f82f553f8183e5f949452be957ff8a
albus-wizard <sophos_thegreek@protonmail.com>
2024-10-17 17:23:24 +0200

    Update for LordyGuide

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const express = require('express');
-const cors = require('cors');
-const { Web3 } = require('web3');
-const bodyParser = require('body-parser');
-const { rateLimit } = require('express-rate-limit')
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const cors = require("cors");
+const { Web3 } = require("web3");
+const bodyParser = require("body-parser");
+const { rateLimit } = require("express-rate-limit");
 
 const app = express();
 const port = 3000;
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(limiter);
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const web3 = new Web3(`${process.env.ALCHEMY_API_URL}/${process.env.ALCHEMY_API_KEY}`);
+const web3 = new Web3(
+  `${process.env.ALCHEMY_API_URL}/${process.env.ALCHEMY_API_KEY}`
+);
 
-const contractABI = require('./helper'); 
+const { nftContractABI, tokenContractABI } = require("./helper.js");
+
 const metadataRarityMappings = {
-  explorer: "https://dev.bord.world/metadatas/explorer.json",
-  knight: "",
-  merchant: "",
-  gangster: "",
-  hunter: "",
-  wizard: "",
-  lord: ""
+  Gold: "https://dev.lordy.guide/metadatas/gold.json",
+  Silver: "https://dev.lordy.guide/metadatas/silver.json",
+  Bronze: "https://dev.lordy.guide/metadatas/bronze.json",
 };
 
-const contractAddress = process.env.NFT_CONTRACT_ADDRESS;
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+const nftContractAddress = process.env.NFT_CONTRACT_ADDRESS;
+const nftContract = new web3.eth.Contract(nftContractABI, nftContractAddress);
+
+const tokenContractAddress = process.env.TOKEN_CONTRACT_ADDRESS;
+const tokenContract = new web3.eth.Contract(
+  tokenContractABI,
+  tokenContractAddress
+);
 
 const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
 web3.eth.accounts.wallet.add(account);
@@ -44,8 +49,8 @@ const users = []; // Dummy users data
 
 // Middleware to authenticate token
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return res.sendStatus(401);
 
@@ -57,11 +62,11 @@ function authenticateToken(req, res, next) {
 }
 
 // Endpoint to generate token upon quiz completion
-app.post('/complete-quiz', (req, res) => {
+app.post("/complete-quiz", (req, res) => {
   const { score } = req.body;
 
   if (score === 100) {
-    const token = jwt.sign({ score: 100 }, JWT_SECRET, { expiresIn: '2m' });
+    const token = jwt.sign({ score: 100 }, JWT_SECRET, { expiresIn: "2m" });
     res.json({ token });
   } else {
     res.status(400).send();
@@ -69,44 +74,100 @@ app.post('/complete-quiz', (req, res) => {
 });
 
 // Mint NFT endpoint (protected)
-app.post('/mint-nft', authenticateToken, async (req, res) => {
-    const { rarity, walletAddress } = req.body;
+app.post("/mint-nft", authenticateToken, async (req, res) => {
+  const { rarity, walletAddress } = req.body;
+  try {
+    //Check if the wallet address already has an NFT with the specified rarity
+    // const hasRarity = await nftContract.methods
+    //   .hasRarity(walletAddress, rarity)
+    //   .call();
+    // if (hasRarity) {
+    //   return res.status(400).send({
+    //     success: false,
+    //     message: "Wallet already owns an NFT with this rarity.",
+    //   });
+    // }
 
-    try {
-      
-      // Check if the wallet address already has an NFT with the specified rarity
-      const hasRarity = await contract.methods.hasRarity(walletAddress, rarity).call();
-      if (hasRarity) {
-        return res.status(400).send({ success: false, message: 'Wallet already owns an NFT with this rarity.' });
+    let metadataUrl = metadataRarityMappings[rarity];
+    // Mint the NFT with the metadata URI
+    const tx = nftContract.methods.safeMint(walletAddress, metadataUrl, rarity);
+    const gas = await tx.estimateGas({ from: account.address });
+    const gasPrice = await web3.eth.getGasPrice();
+    const data = tx.encodeABI();
+    const nonce = await web3.eth.getTransactionCount(account.address);
+
+    const signedTx = await web3.eth.accounts.signTransaction(
+      {
+        to: nftContractAddress,
+        data,
+        gas,
+        gasPrice,
+        nonce,
+        chainId: process.env.CHAINID, //84532 - Sepolia, 8453 - Base
+      },
+      process.env.PRIVATE_KEY
+    );
+
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+    );
+
+    // Now that the NFT is minted, send tokens
+    // Before that, check if sending is enabled
+    const sendTokens = process.env.SENDING_TOKENS;
+    if (sendTokens == "true") {
+      let amountToSend = 0;
+      switch (rarity) {
+        case "Gold":
+          amountToSend = 300;
+          break;
+        case "Silver":
+          amountToSend = 200;
+          break;
+        case "Bronze":
+          amountToSend = 100;
+          break;
+        default:
+          amountToSend = 0;
       }
 
-      let metadataUrl = metadataRarityMappings[rarity];
-      // Mint the NFT with the metadata URI
-      const tx = contract.methods.safeMint(walletAddress, metadataUrl, rarity);
-      const gas = await tx.estimateGas({ from: account.address });
-      const gasPrice = await web3.eth.getGasPrice();
-      const data = tx.encodeABI();
-      const nonce = await web3.eth.getTransactionCount(account.address);
+      if (amountToSend > 0) {
+        const tokenAmount = web3.utils.toBigInt(amountToSend * 10 ** 18);
+        // .mul(web3.utils.toBigInt(10).pow(web3.utils.toBigInt(tokenDecimals)));
+        const tokenTx = tokenContract.methods.transfer(
+          walletAddress,
+          tokenAmount
+        );
+        const tokenGas = await tokenTx.estimateGas({ from: account.address });
+        const tokenGasPrice = await web3.eth.getGasPrice();
+        const tokenData = tokenTx.encodeABI();
+        const tokenNonce = await web3.eth.getTransactionCount(account.address);
 
-      const signedTx = await web3.eth.accounts.signTransaction(
-        {
-          to: contractAddress,
-          data,
-          gas,
-          gasPrice,
-          nonce,
-          chainId: process.env.CHAINID, //84532 - Sepolia, 8453 - Base
-        },
-        process.env.PRIVATE_KEY
-      );
+        const signedTokenTx = await web3.eth.accounts.signTransaction(
+          {
+            to: tokenContractAddress, // Address of the token contract
+            data: tokenData,
+            gas: tokenGas,
+            gasPrice: tokenGasPrice,
+            nonce: tokenNonce,
+            chainId: process.env.CHAINID, // Sepolia or Base
+          },
+          process.env.PRIVATE_KEY
+        );
 
-      const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-      res.status(200).send({ success: true, transactionHash: receipt.transactionHash });
-
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ success: false, error: error.message });
+        const tokenReceipt = await web3.eth.sendSignedTransaction(
+          signedTokenTx.rawTransaction
+        );
+      }
     }
+
+    res
+      .status(200)
+      .send({ success: true, transactionHash: receipt.transactionHash });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, error: error.message });
+  }
 });
 
 app.listen(port, () => {
